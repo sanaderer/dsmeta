fa436201b2cbdd2ab69df5e6371acbe2b2bf140a
sanaderer <sabrinanaderer@hotmail.com>
2022-09-07 11:29:46 -0300

    :tada: Project created

---
+++ backend/src/test/java/io/github/sanaderer/dsmeta/DsmetaApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.sanaderer.dsmeta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsmetaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
